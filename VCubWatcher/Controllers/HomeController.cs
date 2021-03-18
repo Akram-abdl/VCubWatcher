bf558d9a3b162f31f34c17d279e1b10b6efc193a
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-03-18 16:41:42 +0100

    Set remote JSON link to app + display Station list

--- VCubWatcher/Controllers/HomeController.cs
+++ VCubWatcher/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using VCubWatcher.Models;
 
@@ -24,7 +26,8 @@ namespace VCubWatcher.Controllers
         }
         public IActionResult StationsList()
         {
-            return View();
+            var stations = GetBikeStationsFromApi();
+            return View(stations);
         }
         public IActionResult Favorites()
         {
@@ -39,5 +42,23 @@ namespace VCubWatcher.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        private static readonly HttpClient client = new HttpClient();
+        private static List<BikeStations> GetBikeStationsFromApi()
+        {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            using (var client = new HttpClient())
+            {
+                //Interrogation de l'URL censée me retourner les données
+                var response = client.GetAsync("http://api.alexandredubois.com/vcub-backend/vcub.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
+                var result = JsonConvert.DeserializeObject<List<BikeStations>>(stringResult.Result);
+                return result;
+            }
+        }
     }
+      
 }
