81a24ff0c8dda51066aa2a68564d7274cbcbe629
Akram ABDOUL-WAHAB <akram.abdoulwahab@ynov.com>
2021-03-18 15:51:14 +0100

    Create class BikeStations

---
+++ VCubWatcher/Models/BikeStations.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCubWatcher.Models
+{
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<BikeStations>(myJsonResponse); 
+    public class BikeStations
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("bike_count")]
+        public int BikeCount { get; set; }
+
+        [JsonProperty("electric_bike_count")]
+        public int ElectricBikeCount { get; set; }
+
+        [JsonProperty("bike_count_total")]
+        public int BikeCountTotal { get; set; }
+
+        [JsonProperty("slot_count")]
+        public int SlotCount { get; set; }
+
+        [JsonProperty("is_online")]
+        public bool IsOnline { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+    }
+}
